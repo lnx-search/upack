96b778889f70712bc0255aec2f3dfe998804db37
chillfish8 <hburt2003@gmail.com>
2026-01-21 23:11:07 +0000

    Simple packing test

---
+++ benches/pack.rs
@@ -0,0 +1,94 @@
+use std::hint::black_box;
+use divan::Bencher;
+use upack::uint32::X128_MAX_OUTPUT_LEN;
+use upack::X128;
+
+mod utils;
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench(sample_size = 1000, sample_count = 1000)]
+fn bench_pack_avx2_u9_x128(bencher: Bencher) {
+    let sample_data = utils::load_sample_u32_doc_id_data_x128();
+
+    let mut out = [0; X128_MAX_OUTPUT_LEN];
+    bencher
+        .counter(divan::counter::ItemsCount::new(sample_data.len() * X128))
+        .bench_local(|| {
+            for sample in sample_data.iter() {
+                unsafe { upack::uint32::avx2::pack_x128::to_u9(black_box(&mut out), black_box(sample), X128) };
+            }
+        });
+}
+
+#[divan::bench(sample_size = 1000, sample_count = 1000)]
+fn bench_pack_avx512_u9_x128(bencher: Bencher) {
+    let sample_data = utils::load_sample_u32_doc_id_data_x128();
+
+    let mut out = [0; X128_MAX_OUTPUT_LEN];
+    bencher
+        .counter(divan::counter::ItemsCount::new(sample_data.len() * X128))
+        .bench_local(|| {
+            for sample in sample_data.iter() {
+                unsafe { upack::uint32::avx512::pack_x128::to_u9(black_box(&mut out), black_box(sample), X128) };
+            }
+        });
+}
+
+#[divan::bench(sample_size = 1000, sample_count = 1000)]
+fn bench_pack_avx2_u13_x128(bencher: Bencher) {
+    let sample_data = utils::load_sample_u32_doc_id_data_x128();
+
+    let mut out = [0; X128_MAX_OUTPUT_LEN];
+    bencher
+        .counter(divan::counter::ItemsCount::new(sample_data.len() * X128))
+        .bench_local(|| {
+            for sample in sample_data.iter() {
+                unsafe { upack::uint32::avx2::pack_x128::to_u13(black_box(&mut out), black_box(sample), X128) };
+            }
+        });
+}
+
+#[divan::bench(sample_size = 1000, sample_count = 1000)]
+fn bench_pack_avx512_u13_x128(bencher: Bencher) {
+    let sample_data = utils::load_sample_u32_doc_id_data_x128();
+
+    let mut out = [0; X128_MAX_OUTPUT_LEN];
+    bencher
+        .counter(divan::counter::ItemsCount::new(sample_data.len() * X128))
+        .bench_local(|| {
+            for sample in sample_data.iter() {
+                unsafe { upack::uint32::avx512::pack_x128::to_u13(black_box(&mut out), black_box(sample), X128) };
+            }
+        });
+}
+
+#[divan::bench(sample_size = 1000, sample_count = 1000)]
+fn bench_pack_avx2_u17_x128(bencher: Bencher) {
+    let sample_data = utils::load_sample_u32_doc_id_data_x128();
+
+    let mut out = [0; X128_MAX_OUTPUT_LEN];
+    bencher
+        .counter(divan::counter::ItemsCount::new(sample_data.len() * X128))
+        .bench_local(|| {
+            for sample in sample_data.iter() {
+                unsafe { upack::uint32::avx2::pack_x128::to_u17(black_box(&mut out), black_box(sample), X128) };
+            }
+        });
+}
+
+#[divan::bench(sample_size = 1000, sample_count = 1000)]
+fn bench_pack_avx512_u17_x128(bencher: Bencher) {
+    let sample_data = utils::load_sample_u32_doc_id_data_x128();
+
+    let mut out = [0; X128_MAX_OUTPUT_LEN];
+    bencher
+        .counter(divan::counter::ItemsCount::new(sample_data.len() * X128))
+        .bench_local(|| {
+            for sample in sample_data.iter() {
+                unsafe { upack::uint32::avx512::pack_x128::to_u17(black_box(&mut out), black_box(sample), X128) };
+            }
+        });
+}
