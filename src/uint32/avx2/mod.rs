c7aed92dcefbe12204552dce06a6afbfd4cb75d9
chillfish8 <hburt2003@gmail.com>
2026-01-30 14:25:06 +0000

    Add unpacking routines

--- src/uint32/avx2/mod.rs
+++ src/uint32/avx2/mod.rs
@@ -1,11 +1,14 @@
+use std::arch::x86_64::*;
+
 use crate::uint32::{X128_MAX_OUTPUT_LEN, compressed_size};
 use crate::{CompressionDetails, X128};
 
 mod data;
-pub mod modifiers;
+mod modifiers;
 mod pack_x128;
 mod pack_x64_full;
 mod pack_x64_partial;
+mod unpack_x128;
 mod unpack_x64_full;
 mod unpack_x64_partial;
 mod utils;
@@ -44,12 +47,12 @@ pub unsafe fn pack_x128(
 ///
 /// # Safety
 /// - The runtime CPU must support the `avx2` instructions.
-/// - `n` must be less than or equal to 128.
+/// - `pack_n` must be less than or equal to 128.
 pub unsafe fn pack_delta_x128(
+    mut last_value: u32,
     out: &mut [u8; X128_MAX_OUTPUT_LEN],
     block: &mut [u32; X128],
     pack_n: usize,
-    mut last_value: u32,
 ) -> CompressionDetails {
     for v in block.iter_mut() {
         let value = *v;
@@ -66,12 +69,12 @@ pub unsafe fn pack_delta_x128(
 ///
 /// # Safety
 /// - The runtime CPU must support the `avx2` instructions.
-/// - `n` must be less than or equal to 128.
+/// - `pack_n` must be less than or equal to 128.
 pub unsafe fn pack_delta1_x128(
+    mut last_value: u32,
     out: &mut [u8; X128_MAX_OUTPUT_LEN],
     block: &mut [u32; X128],
     pack_n: usize,
-    mut last_value: u32,
 ) -> CompressionDetails {
     for v in block.iter_mut() {
         let value = *v;
@@ -80,4 +83,70 @@ pub unsafe fn pack_delta1_x128(
     }
 
     unsafe { pack_x128(out, block, pack_n) }
+}
+
+#[track_caller]
+#[target_feature(enable = "avx2")]
+/// Pack a block of 128 32-bit integers and write the compressed block to `out`.
+///
+/// # Safety
+/// - The runtime CPU must support the `avx2` instructions.
+/// - `read_n` must be less than or equal to 128.
+/// - `input` buffer must be able to hold the _maximum_ possible length of the packed values for
+///   a given bit length.
+/// - `nbits` must be no greater than `32`.
+pub unsafe fn unpack_x128(
+    nbits: u8,
+    input: &[u8],
+    block: &mut [u32; X128],
+    read_n: usize,
+) -> usize {
+    unsafe { unpack_x128::from_nbits(nbits as usize, input.as_ptr(), block, read_n) };
+    compressed_size(nbits as usize, read_n)
+}
+
+#[track_caller]
+#[target_feature(enable = "avx2")]
+/// Pack a block of 128 32-bit integers and write the compressed block to `out` after
+/// applying Delta encoding.
+///
+/// # Safety
+/// - The runtime CPU must support the `avx2` instructions.
+/// - `read_n` must be less than or equal to 128.
+/// - `input` buffer must be able to hold the _maximum_ possible length of the packed values for
+///   a given bit length.
+/// - `nbits` must be no greater than `32`.
+pub unsafe fn unpack_delta_x128(
+    nbits: u8,
+    last_value: u32,
+    input: &[u8],
+    block: &mut [u32; X128],
+    read_n: usize,
+) -> usize {
+    let bytes_read = unsafe { unpack_x128(nbits, input, block, read_n) };
+    decode_delta(last_value, block);
+    bytes_read
+}
+
+#[track_caller]
+#[target_feature(enable = "avx2")]
+/// Pack a block of 128 32-bit integers and write the compressed block to `out` after
+/// applying Delta-1 encoding.
+///
+/// # Safety
+/// - The runtime CPU must support the `avx2` instructions.
+/// - `read_n` must be less than or equal to 128.
+/// - `input` buffer must be able to hold the _maximum_ possible length of the packed values for
+///   a given bit length.
+/// - `nbits` must be no greater than `32`.
+pub unsafe fn unpack_delta1_x128(
+    nbits: u8,
+    last_value: u32,
+    input: &[u8],
+    block: &mut [u32; X128],
+    read_n: usize,
+) -> usize {
+    let bytes_read = unsafe { unpack_x128(nbits, input, block, read_n) };
+    decode_delta1(last_value, block);
+    bytes_read
 }
