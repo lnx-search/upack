2e0b2e08017198918679221d2b8d6f091025f6ee
chillfish8 <hburt2003@gmail.com>
2026-02-19 23:13:49 +0000

    Use bitwise based operations for packing on full-size blocks

--- src/uint32/avx512/util.rs
+++ src/uint32/avx512/util.rs
@@ -123,23 +123,35 @@ pub(super) fn unpack_u8_to_u16_ordered(data: __m512i) -> [__m512i; 2] {
 /// that can be represented in a [u8] value, meaning any value over [u8::MAX] produces
 /// invalid data.
 pub(super) fn pack_u32_to_u8_unordered(data: [__m512i; 4]) -> __m512i {
-    let packed = pack_u32_to_u16_unordered(data);
-    pack_u16_to_u8_unordered(packed)
+    const OP_MASK: i32 = _MM_TERNLOG_A | _MM_TERNLOG_B | _MM_TERNLOG_C;
+
+    let shift_1 = data[0];
+    let shift_2 = _mm512_slli_epi32::<8>(data[2]);
+    let shift_3 = _mm512_slli_epi32::<16>(data[1]);
+    let shift_4 = _mm512_slli_epi32::<24>(data[3]);
+
+    let mut packed = _mm512_ternarylogic_epi32::<OP_MASK>(shift_1, shift_2, shift_3);
+    packed = _mm512_or_si512(packed, shift_4);
+
+    packed
 }
 
 #[target_feature(enable = "avx512f", enable = "avx512bw")]
 /// Unpack 1 register containing 8-bit elements and produce 4 registers holding
 /// 32-bit elements.
 pub(super) fn unpack_u8_to_u32_unordered(packed: __m512i) -> [__m512i; 4] {
-    let zeroes = _mm512_setzero_si512();
-    let lo_16s = _mm512_unpacklo_epi8(packed, zeroes);
-    let hi_16s = _mm512_unpackhi_epi8(packed, zeroes);
+    let mask = _mm512_set1_epi32(0xFF);
+
+    let shift_1 = packed;
+    let shift_2 = _mm512_srli_epi32::<8>(packed);
+    let shift_3 = _mm512_srli_epi32::<16>(packed);
+    let shift_4 = _mm512_srli_epi32::<24>(packed);
 
     [
-        _mm512_unpacklo_epi16(lo_16s, zeroes),
-        _mm512_unpackhi_epi16(lo_16s, zeroes),
-        _mm512_unpacklo_epi16(hi_16s, zeroes),
-        _mm512_unpackhi_epi16(hi_16s, zeroes),
+        _mm512_and_si512(shift_1, mask),
+        _mm512_and_si512(shift_3, mask),
+        _mm512_and_si512(shift_2, mask),
+        _mm512_and_si512(shift_4, mask),
     ]
 }
 
@@ -154,8 +166,8 @@ pub(super) fn unpack_u8_to_u32_unordered(packed: __m512i) -> [__m512i; 4] {
 /// that can be represented in a [u16] value, meaning any value over [u16::MAX] produces
 /// invalid data.
 pub(super) fn pack_u32_to_u16_unordered(data: [__m512i; 4]) -> [__m512i; 2] {
-    let lo = _mm512_packus_epi32(data[0], data[1]);
-    let hi = _mm512_packus_epi32(data[2], data[3]);
+    let lo = _mm512_or_si512(data[0], _mm512_slli_epi32::<16>(data[1]));
+    let hi = _mm512_or_si512(data[2], _mm512_slli_epi32::<16>(data[3]));
 
     [lo, hi]
 }
@@ -164,27 +176,19 @@ pub(super) fn pack_u32_to_u16_unordered(data: [__m512i; 4]) -> [__m512i; 2] {
 /// Unpack 2 registers containing 16-bit elements and produce 4 registers holding
 /// 32-bit elements.
 pub(super) fn unpack_u16_to_u32_unordered(data: [__m512i; 2]) -> [__m512i; 4] {
-    let zeroes = _mm512_setzero_si512();
-    [
-        _mm512_unpacklo_epi16(data[0], zeroes),
-        _mm512_unpackhi_epi16(data[0], zeroes),
-        _mm512_unpacklo_epi16(data[1], zeroes),
-        _mm512_unpackhi_epi16(data[1], zeroes),
-    ]
-}
+    let mask = _mm512_set1_epi32(0xFFFF);
 
-#[target_feature(enable = "avx512f", enable = "avx512bw")]
-/// Pack 2 sets of registers containing 16-bit elements and produce 1 register holding
-/// 8-bit elements.
-///
-/// The order of elements is **not** maintained.
-///
-/// # Important note on saturation
-/// This routine produces garbage results if the input elements are beyond the maximum value
-/// that can be represented in a [u16] value, meaning any value over [u16::MAX] produces
-/// invalid data.
-pub(super) fn pack_u16_to_u8_unordered(data: [__m512i; 2]) -> __m512i {
-    _mm512_packus_epi16(data[0], data[1])
+    let shift_1 = data[0];
+    let shift_2 = _mm512_srli_epi32::<16>(data[0]);
+    let shift_3 = data[1];
+    let shift_4 = _mm512_srli_epi32::<16>(data[1]);
+
+    [
+        _mm512_and_si512(shift_1, mask),
+        _mm512_and_si512(shift_2, mask),
+        _mm512_and_si512(shift_3, mask),
+        _mm512_and_si512(shift_4, mask),
+    ]
 }
 
 #[target_feature(enable = "avx512f", enable = "avx512bw")]
@@ -223,26 +227,33 @@ pub(super) fn pack_u32_to_u16_split_ordered(data: [__m512i; 4]) -> (__m512i, __m
 /// that can be represented in a [u16] value, meaning any value over [u16::MAX] produces
 /// invalid data.
 pub(super) fn pack_u32_to_u16_split_unordered(data: [__m512i; 4]) -> (__m512i, __m512i) {
-    let packed_u16 = pack_u32_to_u16_unordered(data);
+    const OP_MASK: i32 = _MM_TERNLOG_A | (_MM_TERNLOG_B & _MM_TERNLOG_C);
 
-    let mask = _mm512_set1_epi16(0x00FF);
-    let lo_8bits = and_si512(packed_u16, mask);
-    let hi_8bits = srli_epi16::<8, 2>(packed_u16);
+    let packed = pack_u32_to_u16_unordered(data);
+    let lo_mask = _mm512_set1_epi16(0x00FFu16 as i16);
+    let hi_mask = _mm512_set1_epi16(0xFF00u16 as i16);
 
-    let packed_lo_8bits = pack_u16_to_u8_unordered(lo_8bits);
-    let packed_hi_8bits = pack_u16_to_u8_unordered(hi_8bits);
+    let lo_8bits_1 = _mm512_slli_epi16::<8>(packed[1]);
+    let hi_8bits_1 = _mm512_srli_epi16::<8>(packed[0]);
 
-    (packed_hi_8bits, packed_lo_8bits)
+    let lo_8bits = _mm512_ternarylogic_epi32::<OP_MASK>(lo_8bits_1, packed[0], lo_mask);
+    let hi_8bits = _mm512_ternarylogic_epi32::<OP_MASK>(hi_8bits_1, packed[1], hi_mask);
+
+    (hi_8bits, lo_8bits)
 }
 
 #[target_feature(enable = "avx512f", enable = "avx512bw")]
 /// Unpack 1 register containing 8-bit elements and produce 2 registers holding
 /// 32-bit elements.
 pub(super) fn unpack_u8_to_u16_unordered(data: __m512i) -> [__m512i; 2] {
-    let zeroes = _mm512_setzero_si512();
-    let lo = _mm512_unpacklo_epi8(data, zeroes);
-    let hi = _mm512_unpackhi_epi8(data, zeroes);
-    [lo, hi]
+    let mask = _mm512_set1_epi16(0xFF);
+    let shift_1 = data;
+    let shift_2 = _mm512_srli_epi16::<8>(data);
+
+    [
+        _mm512_and_si512(shift_1, mask),
+        _mm512_and_si512(shift_2, mask),
+    ]
 }
 
 #[target_feature(enable = "avx512f")]
@@ -363,11 +374,11 @@ mod tests {
         let view = unsafe { std::mem::transmute::<[__m512i; 2], [u16; X64]>(packed) };
         assert_eq!(
             view[..16],
-            [0, 1, 2, 3, 16, 17, 18, 19, 4, 5, 6, 7, 20, 21, 22, 23]
+            [0, 16, 1, 17, 2, 18, 3, 19, 4, 20, 5, 21, 6, 22, 7, 23]
         );
         assert_eq!(
             view[16..][..16],
-            [8, 9, 10, 11, 24, 25, 26, 27, 12, 13, 14, 15, 28, 29, 30, 31]
+            [8, 24, 9, 25, 10, 26, 11, 27, 12, 28, 13, 29, 14, 30, 15, 31]
         );
     }
 
@@ -454,11 +465,11 @@ mod tests {
         let view = unsafe { std::mem::transmute::<__m512i, [u8; X64]>(packed) };
         assert_eq!(
             view[..16],
-            [0, 1, 2, 3, 16, 17, 18, 19, 32, 33, 34, 35, 48, 49, 50, 51]
+            [0, 32, 16, 48, 1, 33, 17, 49, 2, 34, 18, 50, 3, 35, 19, 51]
         );
         assert_eq!(
             view[16..][..16],
-            [4, 5, 6, 7, 20, 21, 22, 23, 36, 37, 38, 39, 52, 53, 54, 55]
+            [4, 36, 20, 52, 5, 37, 21, 53, 6, 38, 22, 54, 7, 39, 23, 55]
         );
     }
 
@@ -507,26 +518,6 @@ mod tests {
         not(all(target_feature = "avx512f", target_feature = "avx512bw")),
         ignore
     )]
-    fn test_unpack_u8_u16_unordered() {
-        let mut input = [0; X64];
-        #[allow(clippy::needless_range_loop)]
-        for i in 0..X64 {
-            input[i] = i as u16;
-        }
-
-        let data = unsafe { load_si512x2(input.as_ptr().cast()) };
-        let packed = unsafe { pack_u16_to_u8_unordered(data) };
-        let unpacked = unsafe { unpack_u8_to_u16_unordered(packed) };
-
-        let view = unsafe { std::mem::transmute::<[__m512i; 2], [u16; X64]>(unpacked) };
-        assert_eq!(view, input);
-    }
-
-    #[test]
-    #[cfg_attr(
-        not(all(target_feature = "avx512f", target_feature = "avx512bw")),
-        ignore
-    )]
     fn test_unpack_u8_u16_ordered() {
         let mut input = [0; X64];
         #[allow(clippy::needless_range_loop)]
@@ -540,21 +531,6 @@ mod tests {
 
         let view = unsafe { std::mem::transmute::<[__m512i; 2], [u16; X64]>(unpacked) };
         assert_eq!(view, input);
-    }
-
-    #[test]
-    #[cfg_attr(
-        not(all(target_feature = "avx512f", target_feature = "avx512bw")),
-        ignore
-    )]
-    fn test_pack_u16_to_u8_unordered_layout() {
-        let input: [u16; X64] = std::array::from_fn(|i| i as u16);
-
-        let data = unsafe { load_si512x2(input.as_ptr().cast()) };
-        let packed = unsafe { pack_u16_to_u8_unordered(data) };
-
-        let view = unsafe { std::mem::transmute::<__m512i, [u8; X64]>(packed) };
-        assert_eq!(view, PACK_U16_TO_U8_EXPECTED_UNORDERED_LAYOUT,);
     }
 
     #[test]
